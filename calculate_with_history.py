6a16bd80be27ae0c84d065f59b1606c51567a774
Samruddhi901 <katesamruddhi520@gmail.com>
2026-06-07 13:03:31 +0530

    Add files via upload

---
+++ calculate_with_history.py
@@ -0,0 +1,66 @@
+print("CALCULATOR WITH HISTORY")
+HISTORY_FILE = "history.txt"
+def show_history():
+    file = open(HISTORY_FILE,'r')
+    lines = file.readlines()
+    if len(lines) == 0:
+        print("No history found...!")
+    else:
+        for line in reversed(lines):
+            print(line.strip())
+    file.close()
+
+def clear_history():
+    file = open(HISTORY_FILE,'w')
+    file.close()
+    print('History cleared.')
+
+def save_to_history(equestion,result):
+    file = open(HISTORY_FILE,'a')
+    file.write(equestion + "=" + str(result) + "\n")
+    file.close()
+
+def calculator(user_input):
+    parts = user_input.split()
+    if len(parts) != 3:
+        print("invalid input.")
+        return
+    num1 = float(parts[0])
+    op = parts[1]
+    num2 = float(parts[2])
+
+    if op == "+":
+        result = num1 + num2
+    elif op == "-":
+        result = num1 - num2
+    elif op == "*":
+        result = num1 * num2
+    elif op == "/":
+        if num2 == 0:
+            print('Cannot divide by zero')
+            return
+        result = num1 / num2
+    else:
+        print('Invalid operator.use only +-*/.')
+        return
+    if int(result) == result:
+        result = int(result)
+    print("Rsult: ",result)
+    save_to_history(user_input,result)
+
+def main():
+    print('SIMPLE CALCULATOR')
+    while True:
+        user_input = input("Enter calculation(+-*/) or command (history,clear,exit)")
+        if user_input == 'exit':
+            print('Goodbye')
+            break
+        elif user_input == 'history':
+            show_history()
+        elif user_input == 'clear':
+            clear_history()
+        else:
+            calculator (user_input)
+
+main()            
+                           
